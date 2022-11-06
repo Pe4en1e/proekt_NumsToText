e324f86e78cf56e5898149a004c25f61499c46c6
Roberts Vesīks <alex36254@gmail.com>
2022-11-06 03:48:03 +0300

    main code

---
+++ main.py
@@ -0,0 +1,22 @@
+import inflect
+from googletrans import Translator
+
+translator=Translator()
+p = inflect.engine()
+
+x=input() # принимаем число
+y=(p.number_to_words(x)) # преобразовываем в текст
+ts=translator.translate(str(y), src= 'en', dest= 'ru') # переводим на русский
+
+output=ts.text # берем итоговую строку
+
+print(output.replace(',', ''))
+
+def zmn(x2): 
+    y=(p.number_to_words(x2))
+    ts=translator.translate(str(y), src='en', dest='ru')
+    output=ts.text
+    return output.replace(',', '')
+
+x2=input()
+print(zmn(x2))
---
+++ test.py
@@ -0,0 +1,5 @@
+from googletrans import Translator
+
+ts=Translator()
+
+print(ts.translate('hello', dest='ru'))
