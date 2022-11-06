e950ea08811d7293182acb3e1dd8872f2324641b
Roberts Vesīks <alex36254@gmail.com>
2022-11-07 02:24:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,19 +10,12 @@ from googletrans import Translator
 translator=Translator()
 p = inflect.engine()
 
-print('Введите число:')
-x=input() # принимаем число
-y=(p.number_to_words(x)) # преобразовываем в текст
-ts=translator.translate(str(y), src= 'en', dest= 'ru') # переводим на русский
-output=ts.text # берем итоговую строку
-print(output.replace(',', ''))
-
-def zmn(x): 
+def zmn(x):
     y=(p.number_to_words(x))
     ts=translator.translate(str(y), src='en', dest='ru')
     output=ts.text
-    return output.replace(',', '')
+    return output.replace(',', '').upper()
 
 print('Введите число:')
-x2=input()
-print(zmn(x2))
+x=input()
+print(zmn(x))
