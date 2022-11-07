52170f65af637f4f0e1dc471301ce9829ab4aa83
Roberts <alex36254@gmail.com>
2022-11-07 23:37:38 +0300

    bug fix

--- telegram/main.py
+++ telegram/main.py
@@ -12,6 +12,15 @@ from googletrans import Translator
 translator=Translator()
 p = inflect.engine()
 
+def zmn(x):
+    if int(x)!=10:
+        y=(p.number_to_words(x))
+        ts=translator.translate(str(y), src='en', dest='ru')
+        output=ts.text
+    else:
+        output='ДЕСЯТЬ'
+    return output.replace(',', '').upper()
+
 bot=Bot(token='5661180424:AAEARAsMfPddOCJqFtT4CWXIeewoXLpIzPk')
 dp=Dispatcher(bot, storage=MemoryStorage())
 
@@ -19,7 +28,6 @@ async def on_startup(_):
     for i in track(range(3), description="Включение бота..."):
         time.sleep(1)
     print('[bold green]Бот запущен!')
-
 
 @dp.message_handler(commands=['start'])
 async def command_start(message:types.message):
@@ -29,32 +37,9 @@ async def command_start(message:types.message):
 async def priem(message:types.message):
     num=message.text
     if num.isdigit():
-        def zmn(x): 
-            y=(p.number_to_words(x))
-            ts=translator.translate(str(y), src='en', dest='ru')
-            output=ts.text
-            return output.replace(',', '')
         await bot.send_message(chat_id=message.chat.id, text=zmn(num))
     else:
         await bot.send_message(chat_id=message.chat.id, text='Извини, я работаю только с числами!')
-
-@dp.message_handler()
-async def priem(message:types.message):
-    num=message.text
-    if num.isdigit():
-        def zmn(x):
-            if int(x)!=10:
-                y=(p.number_to_words(x))
-                ts=translator.translate(str(y), src='en', dest='ru')
-                output=ts.text
-            else:
-                output='ДЕСЯТЬ'
-            return output.replace(',', '').upper()
-        await bot.send_message(chat_id=message.chat.id, text=zmn(num))
-    else:
-        await bot.send_message(chat_id=message.chat.id, text='Извини, я работаю только с числами!')
-        
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
-
